16c45ea2812b86f05a70cfb60ba8a9633b84bdf0
Oskar Olortegui <wolorteguiw@gmail.com>
2024-09-07 00:49:41 -0500

    01 Introduction to React Router/ 10 Netlify

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import aboutHero from './about-hero.png'
+import avatarIcon from './avatar-icon.png'
+import homeHero from './home-hero.png'
+import incomeGraph from './income-graph.png'
+import reviewsGraph from './reviews-graph.png'
+
+export {
+  aboutHero,
+  avatarIcon,
+  homeHero,
+  incomeGraph,
+  reviewsGraph
+}
